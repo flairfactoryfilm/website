4e550dc623866e7f8fb4df6cf424209d631c582c
flairfactoryfilm <flairfactoryfilm@gmail.com>
2026-03-08 10:03:43 +0900

    Update About.tsx

--- pages/About.tsx
+++ pages/About.tsx
@@ -170,7 +170,7 @@ const About: React.FC = () => {
       </section>
 
       {/* 2. Why Flair Factory? */}
-      {/* 모바일: h-auto (스티키 해제, 그냥 쌓임), 데스크탑: h-[700vh] (스티키 작동) */}
+      {/* 모바일: h-auto (스티키 해제, 그냥 쌓임), 데스크탑: h-[500vh] (스티키 작동) */}
       <section ref={whySectionRef} className="relative h-auto md:h-[700vh] bg-background z-30">
         
         {/* 모바일: relative & 기본 패딩, 데스크탑: sticky & h-screen */}
